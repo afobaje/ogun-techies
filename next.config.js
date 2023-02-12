4e9374ab120bae16af2e911f258741dbff8a9a6e
davidblaqq <ikukoyidave@gmail.com>
2023-02-13 00:16:13 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    // PAYSTACK:`pk_live_4e0d75c67f662914493729e7caaf1190200a1997`
-    PAYSTACK:`pk_test_a3644451a29f162a0a8935c07dbe376382a99be1`
+    PAYSTACK:`pk_live_4e0d75c67f662914493729e7caaf1190200a1997`
+    
   }
   
 }
